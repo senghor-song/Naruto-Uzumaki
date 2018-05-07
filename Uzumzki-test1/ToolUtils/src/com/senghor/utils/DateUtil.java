e4cab2434925547f647347effa49f8044d59be2d
宋高俊 <songgaojun@ruiec.cn>
2018-05-07 14:23:37 +0800

    增加工具类

---
+++ Uzumzki-test1/ToolUtils/src/com/senghor/utils/DateUtil.java
@@ -0,0 +1,160 @@
+package com.senghor.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateUtil {
+    
+    /**
+     * 时间格式字符串转换为日期类型
+     * <br>yyyy：年  
+     * <br>MM：月  
+     * <br>dd：日  
+     * <br>hh：1~12小时制(1-12)  
+     * <br>HH：24小时制(0-23)  
+     * <br>mm：分  
+     * <br>ss：秒  
+     * <br>S：毫秒  
+     * <br>E：星期几  
+     * <br>D：一年中的第几天  
+     * <br>F：一月中的第几个星期(会把这个月总共过的天数除以7)  
+     * <br>w：一年中的第几个星期  
+     * <br>W：一月中的第几星期(会根据实际情况来算)  
+     * <br>a：上下午标识  
+     * <br>k：和HH差不多，表示一天24小时制(1-24)。  
+     * <br>K：和hh差不多，表示一天12小时制(0-11)。  
+     * <br>z：表示时区  
+     * @param dateStr 传入的时间字符串
+     * @param parseInto 要格式化的类型
+     * @author Senghor<br>
+     * @date 2018年5月7日 下午1:33:53
+     */
+    public static Date getParse(String dateStr,String parseInto){
+        Date date = null;
+        try {
+            date = new SimpleDateFormat(parseInto).parse(dateStr);
+        } catch (ParseException e) {
+            throw new RuntimeException("时间与格式不匹配");
+        }
+        return date;
+    }
+
+    /**
+     * 日期转换为自定义字符串格式
+     * <br>yyyy：年  
+     * <br>MM：月  
+     * <br>dd：日  
+     * <br>hh：1~12小时制(1-12)  
+     * <br>HH：24小时制(0-23)  
+     * <br>mm：分  
+     * <br>ss：秒  
+     * <br>S：毫秒  
+     * <br>E：星期几  
+     * <br>D：一年中的第几天  
+     * <br>F：一月中的第几个星期(会把这个月总共过的天数除以7)  
+     * <br>w：一年中的第几个星期  
+     * <br>W：一月中的第几星期(会根据实际情况来算)  
+     * <br>a：上下午标识  
+     * <br>k：和HH差不多，表示一天24小时制(1-24)。  
+     * <br>K：和hh差不多，表示一天12小时制(0-11)。  
+     * <br>z：表示时区  
+     * @param date 传入的时间
+     * @param parseInto 要格式化的类型
+	 * @author Senghor<br>
+	 * @date 2018年5月7日 下午1:36:30
+	 */
+	public static String getFormat(Date date,String parseInto){
+	    String dateStr = new SimpleDateFormat(parseInto).format(date);
+        return dateStr;
+	}
+	
+    /**
+     * 获取这天时间的00:00:00或23:59:59
+     * @param date 时间
+     * @param flag 0 返回yyyy-MM-dd 00:00:00日期 <br>
+     *             1 返回yyyy-MM-dd 23:59:59日期
+     * @author Senghor<br>
+     * @date 2018年5月7日 下午1:53:16
+     * @return Date
+     */
+    public static Date getWeeHours(Date date, int flag) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        int minute = cal.get(Calendar.MINUTE);
+        int second = cal.get(Calendar.SECOND);
+        //时分秒（毫秒数）
+        long millisecond = hour*60*60*1000 + minute*60*1000 + second*1000;
+        //凌晨00:00:00
+        cal.setTimeInMillis(cal.getTimeInMillis()-millisecond);
+         
+        if (flag == 0) {
+            return cal.getTime();
+        } else if (flag == 1) {
+            //凌晨23:59:59
+            cal.setTimeInMillis(cal.getTimeInMillis()+23*60*60*1000 + 59*60*1000 + 59*1000);
+        }
+        return cal.getTime();
+    }
+
+    /**
+     * 求两个时间之间的时间差
+     * @param startTime 开始时间（减数）
+     * @param endTime 结束时间（被减数）
+     * @param unit 单位   0:秒;  1:分;  2:时;  3:天;<br>
+     * @author Senghor<br>
+     * @date 2018年5月7日 下午1:52:41
+     */
+    public static long getTimeDifference (Date startTime,Date endTime,int unit) {
+    	long timeDifference=0;
+    	long time=endTime.getTime()-startTime.getTime();
+		switch (unit) {
+		case 0:
+			timeDifference=time/1000;//秒
+			break;
+		case 1:
+			timeDifference=time/(1000*60);//分
+			break;
+		case 2:
+			timeDifference=time/(1000*60*60);//时
+			break;
+		case 3:
+			timeDifference=time/(1000*60*60*24);//天
+			break;
+		}
+		return timeDifference;
+	}
+    
+    /**
+     * 对时间进行增加和减少操作
+     * @param date 时间
+     * @param accuracy 0=秒; 1=分; 2=时; 3=天
+     * @param size 要操作的大小(正整数则增加,负数则减少)
+     * @author Senghor<br>
+     * @date 2018年5月7日 下午1:56:35
+     */
+    public static Date getPreTime(Date date,int accuracy, int size){
+        Calendar calendar = Calendar.getInstance();  
+        calendar.setTime(date);
+        switch (accuracy) {
+            case 0:
+                calendar.add(Calendar.SECOND, size);// 时间+size秒
+                break;
+            case 1:
+                calendar.add(Calendar.MINUTE, size);// 时间+size分
+                break;
+            case 2:
+                calendar.add(Calendar.HOUR_OF_DAY, size);// 时间+size小时  
+                break;
+            case 3:
+                calendar.add(Calendar.DAY_OF_MONTH, size);// 时间+size天  
+                break;
+            default:
+                break;
+        }
+        return calendar.getTime();
+    }
+    
+}
